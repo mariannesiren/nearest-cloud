4d6d907a33e2f62694fbaa514eb49e5c2e59d54c
Marianne <marianne.siren@gmail.com>
2020-04-17 18:10:47 +0300

    Fetch clouds from API

--- api/api.py
+++ api/api.py
@@ -1,8 +1,15 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/api')
-def test():
-    return {'test': "works =)"}
+def fetch_clouds():
+    CLOUD_URL = "https://api.aiven.io/v1/clouds"
+    response = requests.get(CLOUD_URL)
+
+    if response.status_code == 200:
+        return {'success': 'true', 'clouds': response.json()}
+    else:
+        return {'success': 'false', 'clouds': ''}
