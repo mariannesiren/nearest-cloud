dc20279a449621903da20964cc1ec3924a26d775
Marianne <marianne.siren@gmail.com>
2020-04-23 13:10:35 +0300

    Add cache to backend

--- api/api.py
+++ api/api.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_caching import Cache
 import requests
 
+config = {
+    "DEBUG": True,
+    "CACHE_TYPE": "simple",
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+
 app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
 
 
 @app.route('/api')
+@cache.cached()
 def fetch_clouds():
     URL = "https://api.aiven.io/v1/clouds"
     response = requests.get(URL)
