f128ea5b1befb757fbf72ae5f1ea23d98eddfa3f
Marianne <marianne.siren@gmail.com>
2020-04-23 13:10:22 +0300

    Rename CLOUD_URL to URL

--- api/api.py
+++ api/api.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/api')
 def fetch_clouds():
-    CLOUD_URL = "https://api.aiven.io/v1/clouds"
-    response = requests.get(CLOUD_URL)
+    URL = "https://api.aiven.io/v1/clouds"
+    response = requests.get(URL)
 
     if response.status_code == 200:
         return {'success': 'true', 'clouds': response.json()}
